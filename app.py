2dfbf0a4d3b65322a9b5f03ee5ed18c9b8271a9c
nguyentruonggiangvbs <nguyentruonggiangvbs@gmail.com>
2026-09-04 10:22:58 +0700

    Restore minimal ASGI entrypoint after adding design API module

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
 from vps_control.app import app
-from vps_control.design_api import router as design_router
-
-# Chèn API thư viện thiết kế trước StaticFiles mount ở "/" để các route /api/design/* được ưu tiên.
-app.router.routes[0:0] = list(design_router.routes)
 
 __all__ = ["app"]
